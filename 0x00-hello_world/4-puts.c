caf6100a497dd554db5e4455895bcb3452f3d65a
mariamkamau <kamaumariam931@gmail.com>
2022-06-30 10:31:09 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,9 @@
 /**
  * main -prints the phrase "Programming is like building a multilingual puzzle
  * Return 0: If exited properly, non-zero otherwise
-
  */
-
 int main(void)
-
 {
-		puts("\"Programming is like building a multilingual puzzle");
-
+ 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
